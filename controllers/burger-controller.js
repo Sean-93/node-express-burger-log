036cebdb2ae9cb82f3d8a069897922ba61413f23
Sean Rooney <seandanielrooney@gmail.com>
2020-03-24 21:03:29 -0700

    got layout looking okay

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -2,18 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('index')
+    const json = {
+        titleLeft: "burgers to be eaten",
+        titleCenter: "eat the burgah!",
+        titleRight: "devoured burgers" , 
+        timing: [
+            {
+              monday: "9:00 - 15:00",
+              tuesday: "10 - 13:00"
+            }
+          ,
+              {
+              monday: "10:00 - 15:00",
+              tuesday: "11 - 19:00"
+            },
+           {
+              monday: "9:00 - 10:00",
+              tuesday: "10 - 21:00"
+            }
+          ]
+    }
+    res.render('index', json)
     // res.send('index', )
 })
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
 
